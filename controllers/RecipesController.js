956cfa2b9e8170b98b0386794cb1b53ec7cbe278
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-01-10 23:13:17 -0300

    Create Show Recipe page

--- controllers/RecipesController.js
+++ controllers/RecipesController.js
@@ -1,15 +1,19 @@
 const data = require('../data.json');
 
 exports.index = (req, res) => {
-  return res.render('admin/recipes', { recipes: data.recipes });
+  return res.render('admin/recipe/index', { recipes: data.recipes });
 };
 
 exports.create = (req, res) => {
-  return res.send("Recipes create");
+  return res.render("Recipes create");
 };
 
 exports.show = (req, res) => {
-  return res.send("Recipes show");
+  const recipeIndex = req.params.id
+  const recipe = [...data.recipes];
+
+  console.log(recipeIndex);
+  return res.render('admin/recipe/show', { recipe: recipe[recipeIndex], recipeIndex: recipeIndex });
 };
 
 exports.edit = (req, res) => {
