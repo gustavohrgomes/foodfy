2460a117e01b2dcd0a874032064ceb685ed73271
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-01-19 23:37:11 -0300

    Implementing post method for create recipe

--- controllers/RecipesController.js
+++ controllers/RecipesController.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const data = require("../data.json");
 
 exports.index = (req, res) => {
@@ -27,7 +28,28 @@ exports.edit = (req, res) => {
 };
 
 exports.post = (req, res) => {
-  return res.send("Recipes post");
+  const keys = Object.keys(req.body);
+
+  for (value of keys) {
+    if (req.body[value] == "") {
+      return res.send("Por favor, preencha todos os campos!");
+    }
+  }
+
+  let { image, ingredients, preparation, information } = req.body;
+
+  data.recipes.push({
+    image,
+    ingredients,
+    preparation,
+    information,
+  });
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
+    if (err) return res.send("Error ao salvar receita :(");
+
+    return res.redirect("/admin/recipes");
+  });
 };
 
 exports.put = (req, res) => {
