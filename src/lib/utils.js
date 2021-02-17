94c31cfb7e9a7f47c72f21516d6446c523d220d9
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-02-16 23:28:12 -0300

    Add Utils for date formatting

---
+++ src/lib/utils.js
@@ -0,0 +1,28 @@
+module.exports = {
+  age(timeStamp) {
+    const today = new Date();
+    const birthDate = new Date(timeStamp);
+
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const month = today.getMonth() - birthDate.getMonth();
+
+    if (month < 0 || (month == 0 && today.getDate() < birthDate.getDate())) {
+      age = age - 1;
+    }
+
+    return age;
+  },
+  date(timeStamp) {
+    const date = new Date(timeStamp);
+
+    const year = date.getUTCFullYear();
+    const month = `0${date.getUTCMonth() + 1}`.slice(-2);
+    const day = `0${date.getUTCDate()}`.slice(-2);
+
+    return {
+      iso: `${year}-${month}-${day}`,
+      birthDay: `${day}/${month}`,
+      format: `${day}/${month}/${year}`,
+    };
+  },
+};
