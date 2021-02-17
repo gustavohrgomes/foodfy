81b3fe6f0db1f1af31accdf1422094525db3c934
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-02-16 23:27:54 -0300

    Create getChef & post method for chefs

--- src/app/controllers/ChefsController.js
+++ src/app/controllers/ChefsController.js
@@ -9,14 +9,26 @@ module.exports = {
   create(req, res) {
     return res.render('admin/chefs/create');
   },
+  post(req, res) {
+    const keys = Object.keys(req.body);
+
+    for (value of keys) {
+      if (req.body[value] == '') {
+        return res.send('Por favor, preencha todos os campos!');
+      }
+    }
+
+    Chefs.create(req.body, function (chef) {
+      return res.redirect(`/admin/chefs/${chef.id}`);
+    });
+  },
   show(req, res) {
-    return res.render('admin/chefs/show');
+    Chefs.getChef(req.params.id, function (chef) {
+      return res.render('admin/chefs/show', { chef });
+    });
   },
   edit(req, res) {
     return res.render('admin/chefs/edit');
-  },
-  post(req, res) {
-    return res.send('criar chef');
   },
   put(req, res) {
     return res.send('atualizar chef');
--- src/app/models/Chefs.js
+++ src/app/models/Chefs.js
@@ -1,8 +1,9 @@
 const db = require('../../config/dbConnection');
+const { date } = require('../../lib/utils');
 
 module.exports = {
   getAllChefs(callback) {
-    const selectFromChefs = `
+    const selectChefsFrom = `
       SELECT
         chefs.*,
         COUNT(recipes) AS total_recipes
@@ -11,10 +12,45 @@ module.exports = {
       GROUP BY chefs.id;
     `;
 
-    db.query(selectFromChefs, (err, results) => {
+    db.query(selectChefsFrom, (err, results) => {
       if (err) throw `Database error! ${err}`;
 
       callback(results.rows);
+    });
+  },
+  getChef(id, callback) {
+    const selectChefFrom = `
+      SELECT
+        chefs.*,
+        COUNT(recipes) AS total_recipes
+      FROM chefs
+      LEFT JOIN recipes ON (chefs.id = recipes.chef_id)
+      WHERE chefs.id = $1
+      GROUP BY chefs.id;
+    `;
+
+    db.query(selectChefFrom, [id], (err, results) => {
+      if (err) throw `Database ${err}!`;
+
+      callback(results.rows[0]);
+    });
+  },
+  create(chef, callback) {
+    const createChef = `
+      INSERT INTO chefs (
+        name,
+        avatar_url
+      )
+      VALUES ($1, $2)
+      RETURNING id
+    `;
+
+    const queryValues = [chef.name, chef.avatar_url];
+
+    db.query(createChef, queryValues, (err, results) => {
+      if (err) throw `Database error! ${err}`;
+
+      callback(results.rows[0]);
     });
   },
 };
