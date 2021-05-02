2fb8bd2262598b749daa4e653f4cbb4ce97a1f3c
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-05-02 17:00:31 -0300

    Separate main routes from index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,13 @@
 const express = require('express');
 const routes = express.Router();
 
-const HomeController = require('../app/controllers/HomeController');
-
+const main = require('./main.routes');
 const chefs = require('./chefs.routes');
 const recipes = require('./recipes.routes');
 
+routes.use(main);
 routes.use('/admin/chefs', chefs);
 routes.use('/admin/recipes', recipes);
-
-// Public Routes
-routes.get('/', HomeController.index);
-routes.get('/about', HomeController.about);
-routes.get('/recipes', HomeController.recipes);
-routes.get('/recipes/search', HomeController.recipes);
-routes.get('/recipes/:id', HomeController.show);
-routes.get('/chefs', HomeController.chefs);
 
 // Alias
 routes.get('/admin', (req, res) => {
