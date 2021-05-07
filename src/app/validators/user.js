3742ecb7e1fad7da7c9514f2f8f6544cbd7fae77
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-05-06 23:00:28 -0300

    Create edit and put validators

--- src/app/validators/user.js
+++ src/app/validators/user.js
@@ -1,3 +1,5 @@
+const { compare } = require('bcryptjs');
+
 const User = require('../models/User');
 
 function checkAllFields(body) {
@@ -34,6 +36,59 @@ async function post(req, res, next) {
   next();
 }
 
+async function edit(req, res, next) {
+  const { id } = req.params;
+
+  const user = await User.findOne({ where: { id } });
+
+  if (user === null || user === undefined) {
+    return res.render('admin/users/index', {
+      error: 'Usuário não encontrado.',
+    });
+  }
+
+  req.user = user;
+
+  next();
+}
+
+async function update(req, res, next) {
+  try {
+    const fillAllFields = checkAllFields(req.body);
+
+    if (fillAllFields) {
+      return res.render('admin/users/edit', fillAllFields);
+    }
+
+    const { id, password } = req.body;
+
+    if (!password) {
+      return res.render('admin/users/edit', {
+        user,
+        error: 'Digite a senha do usuário para atualizar o cadastro.',
+      });
+    }
+
+    const user = await User.findOne({ where: { id } });
+
+    const passed = await compare(password, user.password);
+
+    if (!passed)
+      return res.render('admin/users/edit', {
+        user,
+        error: 'Senha incorreta.',
+      });
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
 module.exports = {
   post,
+  edit,
+  update,
 };
