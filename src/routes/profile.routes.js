100c1c57a8063380b7593496c1db5a0bf96ac879
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-05-12 08:06:51 -0300

    Create profile.routes.js

---
+++ src/routes/profile.routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const routes = express.Router();
+
+const ProfileController = require('../app/controllers/ProfileController');
+
+const ProfileValidator = require('../app/validators/profile');
+
+// Rotas de perfil de um usuário logado
+routes.get('/', ProfileValidator.index, ProfileController.index); // Mostrar o formulário com dados do usuário logado
+routes.put('/', ProfileController.put); // Editar o usuário logado
+
+module.exports = routes;
