d9b7a779d8b351b0bc5fdb7ec1bc1d2b53e4b07e
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-05-13 08:27:41 -0300

    Create put method no Profile Controller

--- src/app/controllers/ProfileController.js
+++ src/app/controllers/ProfileController.js
@@ -1,8 +1,31 @@
+const User = require('../models/User');
+
 module.exports = {
   index(req, res) {
     const { user } = req;
 
     return res.render('admin/profile/index', { user });
   },
-  put(req, res) {},
+  async put(req, res) {
+    try {
+      const { user } = req;
+      let { name, email } = req.body;
+
+      await User.update(user.id, {
+        name,
+        email,
+      });
+
+      return res.render('admin/profile/index', {
+        user: req.body,
+        success: 'Seu cadastro foi atualizado com sucesso. 🚀',
+      });
+    } catch (error) {
+      res.render('admin/profile/index', {
+        user: req.body,
+        error: 'Ops... Algo deu errado! 😓',
+      });
+      throw new Error(error);
+    }
+  },
 };
