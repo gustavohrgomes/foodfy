d35bbf15c2f81ed020f7eaf6de597562ef173bc8
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-05-14 05:24:37 -0300

    Add IsUserAuthenticated middleware to Profile Routes

--- src/routes/profile.routes.js
+++ src/routes/profile.routes.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const routes = express.Router();
+const { IsUserAuthenticated } = require('../app/middlewares/session');
 
 const ProfileController = require('../app/controllers/ProfileController');
 
 const ProfileValidator = require('../app/validators/profile');
 
 // Rotas de perfil de um usuário logado
-routes.get('/', ProfileValidator.index, ProfileController.index); // Mostrar o formulário com dados do usuário logado
-routes.put('/', ProfileValidator.update, ProfileController.put); // Editar o usuário logado
+routes.get(
+  '/',
+  IsUserAuthenticated,
+  ProfileValidator.index,
+  ProfileController.index,
+); // Mostrar o formulário com dados do usuário logado
+routes.put(
+  '/',
+  IsUserAuthenticated,
+  ProfileValidator.update,
+  ProfileController.put,
+); // Editar o usuário logado
 
 module.exports = routes;
