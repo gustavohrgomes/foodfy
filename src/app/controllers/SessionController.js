e2a86df3a5248509a503e8c70f7868e8383b0fd8
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-05-16 18:41:22 -0300

    Send reset passoword email with nodemailer

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,3 +1,8 @@
+const crypto = require('crypto');
+const mailer = require('../../lib/mailer');
+
+const User = require('../models/User');
+
 module.exports = {
   loginForm(req, res) {
     return res.render('session/login');
@@ -14,5 +19,47 @@ module.exports = {
   forgotForm(req, res) {
     return res.render('session/forgot-password');
   },
-  forgot(req, res) {},
+  async forgot(req, res) {
+    const { user } = req;
+
+    try {
+      // token para o usuário
+      const token = crypto.randomBytes(20).toString('hex');
+
+      // criar uma expiração do token
+      let now = new Date();
+      now.setHours(now.getHours() + 1);
+
+      await User.update(user.id, {
+        reset_token: token,
+        reset_token_expires: now,
+      });
+
+      // enviar o email com um link de recuperação de senha
+      await mailer.sendMail({
+        to: user.email,
+        from: 'no-reply@foodfy.com.br',
+        subject: 'Redefinição de senha',
+        html: `
+          <h2>Esqueceu sua senha?</h2>      
+          <p>Não se preocupe, clique no link abaixo para recuperar sua senha</p>
+          <p>
+            <a href="http://localhost:3000/users/password-reset?token=${token}" target="_blank">
+              Recuperar senha
+            </a>
+          </p>
+        `,
+      });
+
+      // avisar o usuário que enviamos o email
+      return res.render('session/forgot-password', {
+        success: `Email de redefinição de senha enviado! 😁 \n Verifique sua caixa de entrada`,
+      });
+    } catch (error) {
+      console.error(error);
+      return res.render('session/forgot-password', {
+        error: 'Ops, algo deu errado... 😥 \n Por favor, tente novamente!',
+      });
+    }
+  },
 };
