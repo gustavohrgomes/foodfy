b0c1955d12d818ad05b4d02939448b9631002546
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-05-16 10:32:58 -0300

    Redirect non admin users to profile

--- src/app/middlewares/session.js
+++ src/app/middlewares/session.js
@@ -8,12 +8,7 @@ function IsUserAuthenticated(req, res, next) {
 
 function IsUserAdmin(req, res, next) {
   if (!req.session.isAdmin) {
-    // TODO: Melhorar a forma como o moddleware valida
-    // se o usuário é admin ou não
-    return res.send({
-      error:
-        'Desculpe, parece que você não possui permissões acessar este recurso. 😅',
-    });
+    return res.redirect('/admin/profile');
   }
 
   next();
