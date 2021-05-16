f8900334b95c138478b11a170401412780d31dbd
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-05-16 10:38:11 -0300

    Deleted commented import

--- src/routes/recipes.routes.js
+++ src/routes/recipes.routes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const routes = express.Router();
 const multer = require('../app/middlewares/multer');
-// const { IsUserAdmin } = require('../app/middlewares/session');
 
 const Recipes = require('../app/controllers/RecipesController');
 
